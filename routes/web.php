fa008bf6f184994389c4bb955113cdfd8818eda7
LisaDrse <r0423168@student.thomasmore.be>
2020-10-07 20:38:22 +0200

    Updated Routes
    Updated Routes/web.php -> added function for data to be send to path of chosen page.

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,25 @@ Route::get('/', function () {
 });
 
 Route::get('/internship', function() {
-    return view('internship');
+    $data = [
+        "title" => "Capgemini is searching for student UX/UI",
+        "informations" => {"We are looking for a talented UX Designer to join our studio in Brussels.", "At Idean we believe and know that great UX can change the world.", "It all starts with awesome people and challenging projects and crafting a work environment that supports thoughtful work and growth."}
+    ];
+    return view('internship', $data);
 });
 
 Route::get('/studentdetail', function() {
-    return view('studentdetail');
+    $data = [
+        "title" => "Filter thru the internships using these filters below",
+        "filters" => {"category", "employees", "skills", "hours", "location"}
+    ];
+    return view('studentdetail', $data);
 });
 
 Route::get('/companydetail', function() {
-    return view('companydetail');
+    $data = [
+        "title" => "Filter thru the students searching for an intership using these filters below",
+        "filters" => {"category", "location"}
+    ];
+    return view('companydetail', $data);
 });
