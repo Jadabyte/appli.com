90236815a885f2f10fabfcde9eff6b58c8a28959
Amber <amber.waltens@hotmail.com>
2020-10-08 16:07:47 +0200

    basic routes and views with h1 (register, login, home)

---
+++ resources/views/register.blade.php
@@ -0,0 +1,1 @@
+<h1>Register here</h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
