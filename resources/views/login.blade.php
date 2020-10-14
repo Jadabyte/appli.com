150af9194318e23ef083d26ca670a8e625c9c775
Amber <amber.waltens@hotmail.com>
2020-10-14 08:05:28 +0200

    basic login form html

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,17 @@
 <h1>Login</h1>
+
+<form action="" method="post">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" placeholder="Enter email" name="email" id="email">
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" name="password" id="password">
+    </div>
+    <input type="submit" value="Sign in">
+    <p>Want to register?</p>
+    <div>
+        <a href = 'register'>Create account</a>
+    </div>
+</form>
