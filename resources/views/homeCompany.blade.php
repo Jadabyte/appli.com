1da7f190526d5c7e95ef00a72d0ba783a3f5b345
Amber <amber.waltens@hotmail.com>
2020-10-16 09:42:26 +0200

    basic html view home student/company without data

--- resources/views/homeCompany.blade.php
+++ resources/views/homeCompany.blade.php
@@ -7,13 +7,49 @@
     <title>Home Company</title>
 </head>
 <body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href ="#">Home</a></li>
-                <li><a href ="#">About</a></li>
-                <li><a href ="#">Contact</a></li>
-                <li><a href ="#">Account</a></li>
-            </ul>
-        </nav>
+<div>
+    <nav>
+        <ul>
+            <li><a href="#"><img src="" alt="logo"></a></li>
+            <li><a href ="#">Home</a></li>
+            <li><a href ="#">About</a></li>
+            <li><a href ="#">Premium</a></li>
+            <li><a href ="#">Contact</a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul>
+            <li><a href="#"><img src="" alt="profile icon"></a></li>
+            <li><input type="text" placeholder="Search.."></li>
+        </ul>
+    </nav>
+</div>
+<h1>Overview of students looking for an internship</h1>
+<p>Filter thru our students based on category (designer, developer or hybrid) and/ or location.</p>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Category</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">Designer</a>
+        <a href="#">Developer</a>
+        <a href="#">Hybrid</a>
     </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Location</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">&lt;5km</a>
+        <a href="#">&gt;5km - &lt;10km</a>
+        <a href="#">&gt;10km</a>
+    </div>
+</div>
+<a href="#">reset filters</a>
+
+<div>
+    <img src="" alt="student profile picture">
+    <p>firstname</p>
+    <p>lastName</p>
+    <p>category</p>
+</div>
+
+</body>
+</html>
--- resources/views/homeStudent.blade.php
+++ resources/views/homeStudent.blade.php
@@ -7,15 +7,65 @@
     <title>Home Student</title>
 </head>
 <body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href ="#">Home</a></li>
-                <li><a href ="#">About</a></li>
-                <li><a href ="#">Contact</a></li>
-                <li><a href ="#">Account</a></li>
-            </ul>
-        </nav>
+<div>
+    <nav>
+        <ul>
+            <li><a href="#"><img src="" alt="logo"></a></li>
+            <li><a href ="#">Home</a></li>
+            <li><a href ="#">About</a></li>
+            <li><a href ="#">Contact</a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul>
+            <li><a href="#"><img src="" alt="profile icon"></a></li>
+            <li><input type="text" placeholder="Search.."></li>
+        </ul>
+    </nav>
+</div>
+<h1>Overview of companies offering an internship</h1>
+<p>Filter thru our companies based on category, number of employees, required skills, hours and/ or location.</p>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Category</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">Designer</a>
+        <a href="#">Developer</a>
+        <a href="#">Hybrid</a>
     </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Employees</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">&lt;10</a>
+        <a href="#">&gt;10 - &lt;50</a>
+        <a href="#">&gt;50</a>
+    </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Skills</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">skill 1</a>
+        <a href="#">skill 2</a>
+        <a href="#">skill 3</a>
+        <a href="#">skill 4</a>
+        <a href="#">skill 5</a>
+    </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Location</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">&lt;5km</a>
+        <a href="#">&gt;5km - &lt;10km</a>
+        <a href="#">&gt;10km</a>
+    </div>
+</div>
+<a href="#">Reset filters</a>
 
-    
+<div>
+    <img src="" alt="company profile picture">
+    <h3>title</h3>
+    <p>description</p>
+</div>
+
+</body>
+</html>
