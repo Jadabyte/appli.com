8a199941589decd4b9e0d9bd8ed6389a381182d6
Amber <amber.waltens@hotmail.com>
2020-10-16 10:21:32 +0200

    import factories

---
+++ database/factories/InternshipFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Internship;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InternshipFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Internship::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->jobTitle,
+            'company_id' => $this->faker->randomDigit
+        ];
+    }
+}
