9f82f11d81891336141d0bff63d8cfd9cdacfc00
LisaDrse <r0423168@student.thomasmore.be>
2020-10-18 18:29:46 +0200

    Update detail.blade.php
    added h1 tag for testing routes

--- resources/views/company/detail.blade.php
+++ resources/views/company/detail.blade.php
@@ -1,25 +1,6 @@
 {{-- LONG DETAILPAGE COMPANY + OUTSTANDING INTERNSHIPS--}}
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href ="#">Home</a></li>
-                <li><a href ="#">About</a></li>
-                <li><a href ="#">Contact</a></li>
-                <li><a href ="#">Account</a></li>
-            </ul>
-        </nav>
-    </div>
 
-    <h1>{{$name}}</h1>
+{{--     <h1>{{$name}}</h1>
 
     <img src="#" alt="">Image company
 
@@ -32,8 +13,6 @@
         <li>{{$internship}} <a href ="#">More</a></li>
         @endforeach
 
-    </ul>
+    </ul> --}}
 
-    <footer>Copyright Appli</footer>
-</body>
-</html>
+    <h1>Detail</h1>
