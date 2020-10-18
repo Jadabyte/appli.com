34a8c29ce5cb2cc9d4b3f12e2645f77f79b0bc87
LisaDrse <r0423168@student.thomasmore.be>
2020-10-18 18:30:25 +0200

    Update index.blade.php
    added h1 tag for testing routes

--- resources/views/internship/index.blade.php
+++ resources/views/internship/index.blade.php
@@ -2,12 +2,4 @@
 
     <h1>Internships</h1>
 
-    <ul>
-        @foreach ($internships as $internship)
-            <li><a href="/internship/{{$internship->id}}">{{$internship->title}}</a></li>
-        @endforeach
-
-    </ul>
-
-    <input type="submit" value="Apply">
 
