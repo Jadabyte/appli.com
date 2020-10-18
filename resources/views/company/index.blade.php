4b77cfbcf6f34bb9cebda435e5a8b53674fab1bb
LisaDrse <r0423168@student.thomasmore.be>
2020-10-18 18:30:10 +0200

    Update index.blade.php
    added code for index created by Amber

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,21 +1,28 @@
 {{-- LIST OF ALL COMPANIES  --}}
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Company</title>
-</head>
-<body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href ="#">Home</a></li>
-                <li><a href ="#">About</a></li>
-                <li><a href ="#">Contact</a></li>
-                <li><a href ="#">Account</a></li>
-            </ul>
-        </nav>
+<h1>Overview of students looking for an internship</h1>
+<p>Filter thru our students based on category (designer, developer or hybrid) and/ or location.</p>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Category</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">Designer</a>
+        <a href="#">Developer</a>
+        <a href="#">Hybrid</a>
     </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Location</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">&lt;5km</a>
+        <a href="#">&gt;5km - &lt;10km</a>
+        <a href="#">&gt;10km</a>
+    </div>
+</div>
+<a href="#">reset filters</a>
+
+<div>
+    <img src="" alt="student profile picture">
+    <p>firstname</p>
+    <p>lastName</p>
+    <p>category</p>
+</div>
