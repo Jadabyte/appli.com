07162cb8be5e994cdc7f6792e9f9814e625af68f
LisaDrse <r0423168@student.thomasmore.be>
2020-10-18 18:33:09 +0200

    Update index.blade.php
    added code for overview companies for index page students

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,1 +1,46 @@
 {{-- LIST OF STUDENTS --}}
+
+<h1>Overview of companies offering an internship</h1>
+<p>Filter thru our companies based on category, number of employees, required skills, hours and/ or location.</p>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Category</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">Designer</a>
+        <a href="#">Developer</a>
+        <a href="#">Hybrid</a>
+    </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Employees</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">&lt;10</a>
+        <a href="#">&gt;10 - &lt;50</a>
+        <a href="#">&gt;50</a>
+    </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Skills</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">skill 1</a>
+        <a href="#">skill 2</a>
+        <a href="#">skill 3</a>
+        <a href="#">skill 4</a>
+        <a href="#">skill 5</a>
+    </div>
+</div>
+<div class='dropdown'>
+    <button onclick="myFunction()" class="dropbtn">Location</button>
+    <div id="myDropdown" class="dropdown-content">
+        <a href="#">&lt;5km</a>
+        <a href="#">&gt;5km - &lt;10km</a>
+        <a href="#">&gt;10km</a>
+    </div>
+</div>
+<a href="#">Reset filters</a>
+
+<div>
+    <img src="" alt="company profile picture">
+    <h3>title</h3>
+    <p>description</p>
+    <a href="#">more</a>
+</div>
