262b710ef72dee9a57cc9bb097e4a87d488e4dd6
LisaDrse <r0423168@student.thomasmore.be>
2020-10-22 14:16:36 +0200

    Update InternshipSeeder.php
    added hardcoded but didn't work
    needs to be updated by Wannes

--- database/seeders/InternshipSeeder.php
+++ database/seeders/InternshipSeeder.php
@@ -13,6 +13,45 @@ class InternshipSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Internship::factory()->count(6)->create();
+        //\App\Models\Internship::factory()->count(25)->create();
+        /* $internship1 = new \App\Models\Internship();
+        $internship1 ->id = '1';
+        $internship1 ->title = 'Full Stack Developer';
+        $internship1 ->description = 'We are searching for talented software engineering interns who have experience with web development. Ideally, candidates should have experience with our technologies: React, Redux, python, Node, Typescript. Since we are such a small company, interns must be able to build new features independently and be proactive about their own learning.';
+        $internship1 ->category='Development';
+        $internship1 ->requirements='- Currently an undergrad majoring in Computer Science at a 4-year university
+        - Has contributed to a live website or web application
+        - Completed at least one production-level internship
+        - Interested in creative tools, social media, and entrepreneurship
+        - Comfortable with Javascript, React, and python
+        - Proactive: Identifies problems and solutions';
+        $internship1 ->company_id= '3';
+        $internship1 ->availability='1';
+        $internship1 ->save();
+
+        $internship2 = new \App\Models\Internship();
+        $internship2 ->id = '2';
+        $internship2 ->title = 'UX/ UI Designer';
+        $internship2 ->description = 'Copart is looking for UX/ UI Intern, we are seeking a passionate, opinionated and creative individual who can design web applications from the ground up. The successful candidate will be a designer who understands web strengths and constraints, and builds pixel perfect solutions.';
+        $internship2 ->category='Design';
+        $internship2 ->requirements='- Develop well-structured web applications using modern, cutting edge JavaScript libraries/frameworks like ReactJS, ReactNative, Node.js etc.
+        - Work with UX designers and other developers to build modular, reusable components and compelling web applications.
+        - Passion for creating experiences that users will rave about.
+        - Learn quickly and be productive in a highly collaborative, lightning-fast environment.
+        - Follow and promote best practices';
+        $internship2 ->company_id= '7';
+        $internship2 ->availability='1';
+        $internship2 ->save();
+
+        $internship3 = new \App\Models\Internship();
+        $internship3 ->id = '3';
+        $internship3 ->title = 'Hybrid';
+        $internship3 ->description = 'Front-End Development/Designer';
+        $internship3 ->category='Hybrid';
+        $internship3 ->requirements='- CSS3, Html, JavaScript, NodeJS
+        - Sketch, Adobe Illustrator, Procreate';
+        $internship3 ->company_id= '4';
+        $internship3 ->availability='0';
+        $internship3 ->save(); */
     }
 }
