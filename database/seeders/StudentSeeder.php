e3e755475502a32702b641cd7694899d2e27c9d1
LisaDrse <r0423168@student.thomasmore.be>
2020-10-22 14:17:21 +0200

    Create StudentSeeder.php
    added Students seeder

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Students::factory()->count(20)->create();
+    }
+}
