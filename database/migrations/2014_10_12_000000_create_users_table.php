637205079369c1b92bdd5af84a20a4ad81905264
LisaDrse <r0423168@student.thomasmore.be>
2020-10-22 14:17:10 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,17 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->increments('id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('category');
-            $table->string('portfolio');
-            $table->string('street');
-            $table->integer('house_number');
-            $table->integer('postal_code');
-            $table->string('city');
             $table->rememberToken();
             $table->timestamps();
         });
