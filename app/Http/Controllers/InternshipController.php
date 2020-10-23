e98072347492eb058220610fb239d6c0f76257c5
LisaDrse <r0423168@student.thomasmore.be>
2020-10-23 22:10:21 +0200

    Update InternshipController.php
    update controllers

--- app/Http/Controllers/InternshipController.php
+++ app/Http/Controllers/InternshipController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Internship;
+
 class InternshipController extends Controller
 {
     public function index(){
-
-        return view('internship/index');
+        $data['internships'] = \DB::table('internships')->get();
+        return view('internship/index', $data);
     }
 
-    public function detail(){
-        return view('internship/detail');
+    public function show(\App\Models\Internship $internship){
+        $internship = $internship;
+       // dd($internship);
+
+        return view('internship/show');
+    }
+
+    public function detail($id){
+        return view('internship.detail', ['internships' => Internship::findOrFail($id)]);
     }
 
     public function create(){
+
         return view('internship/createInternship');
     }
 }
