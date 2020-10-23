cca21e41d158a7c1a8390a11a493a2aa187321a0
LisaDrse <r0423168@student.thomasmore.be>
2020-10-23 22:10:27 +0200

    Update StudentController.php
    update controllers

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,18 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class StudentController extends Controller
 {
     public function index(){
 
+        $data['students'] = \DB::table('students')->get();
+        return view('student/index', $data);
+    }
+
+    public function show($student){
+        $data['students'] = \DB::table('students')->get();
+        return view('student/show', $data);
+    }
+
+    public function register(){
+
         return view('registerStudent');
-        return view('student/index');
+
     }
 
-    public function detail(){
-        return view('student/detail');
+    public function login(){
+
+        return view('login');
     }
 
+    public function detail($id){
+        return view('student.detail', ['users' => User::findOrFail($id)]);
+    }
 
 
 }
