1e5d1c37c8a844db06f00223267bfe35680ae323
LisaDrse <r0423168@student.thomasmore.be>
2020-11-30 12:46:30 +0100

    Update create.blade.php
    added layout for internship/create

--- resources/views/internship/create.blade.php
+++ resources/views/internship/create.blade.php
@@ -7,15 +7,8 @@
 @section('content')
 
     @component('components/navigation')@endcomponent
-    <section id="create">
-        <div class="container" id="containerCreate">
-            <div id="createExplain" class="block-heading">
-                <h1 id="h1" class="nameStudent" style="font-size: 30px;">Create a new internship</h1>
-                <p>To create a new internship for your company, fill in this form and press "add internship".</p>
-            </div>
-        @component('components/createInternship')@endcomponent
-        </div>
-    </section>
+
+    @component('components/createInternship')@endcomponent
 
     @component('components/footer')@endcomponent
 @endsection
