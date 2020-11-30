a12ac42c33d20842bd2f356ba84ce2492f924b75
LisaDrse <r0423168@student.thomasmore.be>
2020-11-30 12:46:46 +0100

    Update index.blade.php
    added layout for index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,30 @@
-{{-- PUBLIC HOME PAGE --}}
+@extends('layouts/appli')
 
-<h1>Homepagina Appli</h1>
-<p>about txt</p>
-<a href="register">Register</a>
-<a href="login">Login</a>
+@section('title')
+    Homepagina
+@endsection
+
+@section('content')
+<img class="indexImage" src="/applibranding/logoAppli.svg">
+    <h1 class="headerOne">Welcome to Appli</h1>
+    <div class="indexContainer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="indexLogin">
+                        <a class="indexLoginLink" href="login">
+                            <p class="indexLoginLinkP">Login here</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="indexRegister">
+                        <a class="indexRegisterLink" href="register">
+                            <p class="indexRegisterLinkP">Register here</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
