310e2265e47b3a1a3a24fcc62f8571fb6ac3b499
TERRAKID <wannes.verboven@outlook.com>
2020-12-26 17:12:22 +0100

    [fix] Make login and register form same style

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,45 +6,37 @@
 
 @section('content')
 
-<!-- Start: Login Form Dark -->
-<div class="login-dark">
-    <img class="img-fluid logo" src="/applibranding/logoAppli.svg?h=60d8998b2af02b7c83c7ce77b565694b" loading="lazy">
+    <div class="login-dark">
+        <img class="img-fluid logo" src="/applibranding/logoAppli.svg?h=60d8998b2af02b7c83c7ce77b565694b" loading="lazy">
+        <form method="post" action="">
+            <h2 class="text-center headerTwo">Login</h2>
 
-    <h2 class="headerTwo" style="margin-left:45%; margin-top:5%;">Login here</h2>
+            @csrf
+            @if( $flash = session('message') )
+                <div class="alert alert-success">{{ $flash }}</div>
+            @endif
 
-    <form method="post">
-    @csrf
-        <h2 class="sr-only">Login Form</h2>
-        @if( $flash = session('message') )
-            <div class="alert alert-success">{{ $flash }}</div>
-        @endif
+            @if( $flash = session('error') )
+                <div class="alert alert-danger">{{ $flash }}</div>
+            @endif
 
-        @if( $flash = session('error') )
-            <div class="alert alert-danger">{{ $flash }}</div>
-        @endif
+            @if( $errors->any())
+                <ul class="alert alert-danger">
+                @foreach($errors->all() as $e)
+                    <li>{{ $e }}</li>
+                @endforeach
+                </ul>
+            @endif
 
-        @if( $errors->any())
-            <ul class="alert alert-danger">
-            @foreach($errors->all() as $e)
-                <li>{{ $e }}</li>
-            @endforeach
-            </ul>
-        @endif
-        <div class="illustration">
-            <i class="icon ion-ios-locked-outline"></i>
-        </div>
-        <div class="form-group">
-            <input class="form-control" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
-        </div>
-        <div class="form-group">
-            <input class="form-control" type="password" name="password" placeholder="Password">
-        </div>
-        <div class="form-group" id="form-button-group">
-            <button class="btn btn-primary btn-block btn" id="button" type="submit">Log In</button>
-        </div>
+            <div class="form-group">
+                <input class="form-control" type="email" name="email" placeholder="Email" aria-describedby="emailHelp" value="{{ old('email') }}">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" name="password" placeholder="Password">
+            </div>
+            <button class="btn btn-primary btn-block" id="loginbutton" type="submit">Log in</button>
+            <a id="already" class="already" href="register">New here? Go to register page.</a>
+        </form>
+    </div>
 
-        <a class="forgot" href="register">New here? Go to register page.</a>
-    </form>
-</div>
-<!-- End: Login Form Dark -->
 @endsection
