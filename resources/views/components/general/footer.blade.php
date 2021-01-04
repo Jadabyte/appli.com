fe7944f39235d39c27e292e3de7336d6ca9a06dd
LisaDrse <r0423168@student.thomasmore.be>
2021-01-04 21:05:41 +0100

    Update footer.blade.php

--- resources/views/components/general/footer.blade.php
+++ resources/views/components/general/footer.blade.php
@@ -1,4 +1,4 @@
-<footer id="footerpad" class="footerCreate">
+<footer id="footerpad" class="footerCreate" style="margin: auto">
     <div class="container" id="containerFooter">
         <div class="row">
             <div class="col-md-6 col-lg-8 mx-auto">
