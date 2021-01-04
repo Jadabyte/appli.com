b93320ee277e38003fcb695375020adf30b31f11
LisaDrse <r0423168@student.thomasmore.be>
2021-01-04 20:44:31 +0100

    added new layout
    added layout consisting of components to students/index

---
+++ resources/views/components/student/filters.blade.php
@@ -0,0 +1,74 @@
+<div style="margin-top: 50px;margin-bottom: 50px;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="text-center headerOne">Possible internships</h1>
+                    <p>Filter thru our companies based on category, number of employees, required skills, hours and/ or location.</p>
+                </div>
+            </div>
+    <div class="filter">
+        <form>
+            <select>
+                <option value="">Category</option>
+                <option value="">Hybrid</option>
+                <option value="">Designer</option>
+                <option value="">Devigner</option>
+            </select>
+            <select>
+                <option value="">Location</option>
+                <option value="">&lt; 5km</option>
+                <option value="">&gt; 5km / &lt; 10km</option>
+                <option value="">&gt; 10km</option>
+            </select>
+            <select>
+                <option value="">Skills</option>
+                    <option value="">Laravel</option>
+                    <option value="">Linux</option>
+                    <option value="">PHP</option>
+                    <option value="">CSS</option>
+                    <option value="">HTML</option>
+                    <option value="">Javascript</option>
+                    <option value="">NodeJS</option>
+                    <option value="">Sass</option>
+                    <option value="">ASP.NET Core</option>
+                    <option value="">React</option>
+                    <option value="">JSON</option>
+                    <option value="">API's</option>
+                    <option value="">Git</option>
+                    <option value="">GitHub</option>
+                    <option value="">VueJS</option>
+                    <option value="">Websockets</option>
+                    <option value="">Gulp</option>
+                    <option value="">Parcel</option>
+                    <option value="">Babel</option>
+                    <option value="">Java</option>
+                    <option value="">MongoDB</option>
+                    <option value="">CMS</option>
+                    <option value="">Drupal</option>
+                    <option value="">Arduino</option>
+                    <option value="">Information Architecture</option>
+                    <option value="">User Interface Design</option>
+                    <option value="">User Experience</option>
+                    <option value="">Sketch</option>
+                    <option value="">Procreate</option>
+                    <option value="">Adobe Illustrator</option>
+                    <option value="">Adobe Photoshop</option>
+                    <option value="">Adobe Indesign</option>
+                    <option value="">Adobe Premiere Pro</option>
+                    <option value="">Adobe XD</option>
+                    <option value="">Wonda</option>
+                    <option value="">Kanban</option>
+                    <option value="">SCRUM</option>
+                    <option value="">Agile</option>
+                    <option value="">Trello</option>
+            </select>
+            <select>
+                <option value="">Company Size</option>
+                <option value="">&lt; 10 employees</option>
+                <option value="">&gt; 10 employees / &lt; 50 employees</option>
+                <option value="">&gt; 50 employees</option>
+            </select>
+            <a class="resetFilter" href="#">Reset filters</a>
+        </form>
+    </div>
+</div>
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -5,58 +5,7 @@
 @endsection
 
 @section('content')
-<section>
-    <h1>Overview of companies offering an internship</h1>
-    <p>Filter thru our companies based on category, number of employees, required skills, hours and/ or location.</p>
-    <div class='dropdown'>
-        <button onclick="myFunction()" class="dropbtn">Category</button>
-        <div id="myDropdown" class="dropdown-content">
-            <a href="#">Designer</a>
-            <a href="#">Developer</a>
-            <a href="#">Hybrid</a>
-        </div>
-    </div>
-    <div class='dropdown'>
-        <button onclick="myFunction()" class="dropbtn">Employees</button>
-        <div id="myDropdown" class="dropdown-content">
-            <a href="#">&lt;10</a>
-            <a href="#">&gt;10 - &lt;50</a>
-            <a href="#">&gt;50</a>
-        </div>
-    </div>
-    <div class='dropdown'>
-        <button onclick="myFunction()" class="dropbtn">Skills</button>
-        <div id="myDropdown" class="dropdown-content">
-            <a href="#">skill 1</a>
-            <a href="#">skill 2</a>
-            <a href="#">skill 3</a>
-            <a href="#">skill 4</a>
-            <a href="#">skill 5</a>
-        </div>
-    </div>
-    <div class='dropdown'>
-        <button onclick="myFunction()" class="dropbtn">Location</button>
-        <div id="myDropdown" class="dropdown-content">
-            <a href="#">&lt;5km</a>
-            <a href="#">&gt;5km - &lt;10km</a>
-            <a href="#">&gt;10km</a>
-        </div>
-    </div>
-    <a href="#">Reset filters</a>
-
-
-    @foreach ($internships as $i)
-        <div class="card-group">
-            <div class="card">
-                <img class="card-img-top" src="..." alt="logo company image">
-                    <div class="card-body">
-                        <h3 class="card-title">{{$i->title}}</h3>
-                        <h4 class="card-title">company id:{{$i->company_id}} (eigenlijk moet hier naam van bedrijf komen)</h4>
-                        <p class="card-text">{{$i->description}}</p>
-                        <a href="/internship/{{$i->id}}">More</a>
-                    </div>
-            </div>
-        </div>
-    @endforeach
-</section>
+    @component('components/navigation')@endcomponent
+    @component('components/student/filters')@endcomponent
+    @component('components/internship/showInternships')@endcomponent
 @endsection
