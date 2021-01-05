2e09f524b4912059357b332a487e1539a2bd11ed
LisaDrse <r0423168@student.thomasmore.be>
2021-01-05 13:52:26 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<img class="indexImage" src="/applibranding/logoAppli.svg">
+<img class="indexImage" src="/applibranding/logoAppli.svg" style="margin:auto;">
     <h1 class="headerOne">Welcome to Appli</h1>
     <div class="indexContainer">
         <div class="container">
