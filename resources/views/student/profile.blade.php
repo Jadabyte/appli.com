1b50f7f1ad8083be9b064cb224dadad24fcefa57
LisaDrse <r0423168@student.thomasmore.be>
2021-01-05 17:38:50 +0100

    Update profile.blade.php
    updated css on student profile

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -5,16 +5,15 @@
 
 @section('content')
 
-    @component('components/general/navigation')@endcomponent
+@component('components/general/navigation')@endcomponent
 
-    <h1 class="headerOne" style="text-align:center; margin:2%;">My profile</h1>
-    <section>
-        <div class="profileForm"  id="profile" style="margin-top:30%">
+<h1 class="headerOne" style="text-align:center; margin:2%;">My profile</h1>
 
-{{-- <div class="container profile profile-view" id="profile"> --}}
-
-            <form class="profileContainer" method="post" action="/student/create" enctype="multipart/form-data">
-                @csrf
+<section>
+    <h3 class="headerThree" style="margin-top:2%">Information</h3>
+    <div class="profileForm"  id="profile" style="margin-top:30%">
+        <form class="profileContainer" method="post" action="/student/create" enctype="multipart/form-data">
+            @csrf
                 @if( $flash = session('message') )
                     <div class="alert alert-success">{{ $flash }}</div>
                 @endif
@@ -39,68 +38,75 @@
                 </div>
                 @endisset
 
-        <div class="form-row" style="margin-top:-5%">
-                    <div class="col">
-                        <div class="form-group" style="color:#011C3A;">
-                            <label for="firstName" class="profileFieldLabel">Firstname</label>
-                            <input class="form-control profileGroup" type="text" name="firstName" placeholder="Firstname" value="{{ $user->firstName }}">
-                        </div>
-                        <div class="form-group" style="color: #011C3A;">
-                            <label for="lastName" class="profileFieldLabel">Lastname</label>
-                            <input class="form-control profileGroup" type="text" name="lastName" placeholder="Lastname" value="{{ $user->lastName }}">
-                        </div>
-                        <div class="form-group" style="color: #011C3A;">
-                            <label for="mobile" class="profileFieldLabel">Mobile phone</label>
-                            <input class="form-control profileGroup" type="tel" name="mobile" placeholder="Mobile phone" value="@isset($user->student->mobile){{ $user->student->mobile }}@else{{ old('mobile') }}@endisset">
-                        </div>
+            <div class="form-row" style="margin-top:-5%">
+                <div class="col">
+                    <div class="form-group" style="color:#011C3A;">
+                        <label for="firstName" class="profileFieldLabel">Firstname</label>
+                        <input class="form-control profileGroup" type="text" name="firstName" placeholder="Firstname" value="{{ $user->firstName }}">
                     </div>
-
-                    <div class="col">
-                        <div class="avatar">
-                            <div class="avatar-bg center" style="margin-left:35%;"></div>
-                                <div style="margin-top:1%; color: #011C3A;">
-                                    <label for="picture" class="profileFieldLabel">Profile picture</label>
-                                    <input type="file" class="form-control" name="picture" style="color: #011C3A;" aria-describedby="fileHelp">
-                                    <p class="infoSearch" id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</p>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="lastName" class="profileFieldLabel">Lastname</label>
+                        <input class="form-control profileGroup" type="text" name="lastName" placeholder="Lastname" value="{{ $user->lastName }}">
                     </div>
-
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label for="biography" class="profileFieldLabel">Biography</label>
-                                <input class="form-control profileGroup" type="text" name="biography" placeholder="Biography">@isset($user->student->biography){{ $user->student->biography }}@else{{ old('biography') }}@endisset>
-                            </div>
-                        </div>
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="mobile" class="profileFieldLabel">Mobile phone</label>
+                        <input class="form-control profileGroup" type="tel" name="mobile" placeholder="Mobile phone" value="@isset($user->student->mobile){{ $user->student->mobile }}@else{{ old('mobile') }}@endisset">
                     </div>
+                </div>
 
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label for="portfolio" class="profileFieldLabel">Portfolio website</label>
-                                <input class="form-control profileGroup" type="url" name="portfolio" placeholder="Portfolio link" value="@isset($user->student->portfolio){{ $user->student->portfolio }}@else{{ old('portfolio') }}@endisset">
+                <div class="col">
+                    <div class="avatar">
+                        <div class="avatar-bg center" style="margin-left:35%;"></div>
+                            <div style="margin-top:1%; color: #011C3A;">
+                                <label for="picture" class="profileFieldLabel">Profile picture</label>
+                                <input type="file" class="form-control" name="picture" style="color: #011C3A;" aria-describedby="fileHelp">
+                                <p class="infoSearch" id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</p>
                             </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label for="linkedin" class="profileFieldLabel">LinkedIn</label>
-                                <input class="form-control profileGroup" type="url" name="linkedin" placeholder="LinkedIn" value="@isset($user->student->LinkedIn){{ $user->student->LinkedIn }}@else{{ old('linkedin') }}@endisset">
-                            </div>
-                        </div>
-                    </div>
-
-                <div class="form-row">
-                    <div class="col">
-                        <div class="form-group" style="color: #011C3A;">
-                            <label for="category" class="profileFieldLabel">Category</label>
-                            @foreach ($categories as $category)
-                                <option @if(!empty($user->student->category_id) && $category->id === $user->student->category_id){{ 'selected' }}@endif value="{{ $category->id }}">{{ $category->title }}</option>
-                            @endforeach
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="biography" class="profileFieldLabel">Biography</label>
+                        <input class="form-control profileGroup" type="text" name="biography" placeholder="Biography">@isset($user->student->biography){{ $user->student->biography }}@else{{ old('biography') }}@endisset
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="portfolio" class="profileFieldLabel">Portfolio website</label>
+                        <input class="form-control profileGroup" type="url" name="portfolio" placeholder="Portfolio link" value="@isset($user->student->portfolio){{ $user->student->portfolio }}@else{{ old('portfolio') }}@endisset">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="linkedin" class="profileFieldLabel">LinkedIn</label>
+                        <input class="form-control profileGroup" type="url" name="linkedin" placeholder="LinkedIn" value="@isset($user->student->LinkedIn){{ $user->student->LinkedIn }}@else{{ old('linkedin') }}@endisset">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="category" class="profileFieldLabel">Category</label>
+                        <form class="form-inline">
+                                <div class="form-group">
+                                    <select class="form-control descInput">
+                                        @foreach ($categories as $category)
+                                            <option @if(!empty($user->student->category_id) && $category->id === $user->student->category_id){{ 'selected' }}@endif value="{{ $category->id }}">{{ $category->title }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
             <div class="form-row" style="text-align:right;">
                 <div class="col offset-xl-0">
@@ -108,63 +114,65 @@
                     <button class="btn btn-primary btnApproved" type="button" style="margin-right:5%;">Save</button>
                 </div>
             </div>
-    </form>
-</div>
+        </form>
+    </div>
 </section>
 
-    <section style="margin-top:65%;">
-        <h3 class="headerThree">Accountdetails</h3>
-        <div class="profileForm" style="margin-top:17.5%">
-            <form class="profileContainer" method="post" action="" style="margin-top:0%;">
-                @csrf
+<section style="margin-top:65%;">
+    <h3 class="headerThree">Accountdetails</h3>
+    <div class="profileForm" style="margin-top:17.5%">
+        <form class="profileContainer" method="post" action="" style="margin-top:0%;">
+            @csrf
                 <div class="form-row" style="margin-top:-10%">
-                            <div class="col">
-                                <div class="form-group" style="color: #011C3A;">
-                                    <label for="firstName" class="profileFieldLabel">Firstname</label>
-                                    <input class="form-control profileGroup" type="text" name="firstName" placeholder="Firstname" value="{{ $user->firstName }}">
-                                </div>
-                            </div>
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label for="firstName" class="profileFieldLabel">Firstname</label>
+                            <input class="form-control profileGroup" type="text" name="firstName" placeholder="Firstname" value="{{ $user->firstName }}">
+                        </div>
+                    </div>
 
-                            <div class="col">
-                                <div class="form-group" style="color: #011C3A;">
-                                    <label for="lastName" class="profileFieldLabel">Lastname</label>
-                                    <input class="form-control profileGroup" type="text" name="lastName" placeholder="Lastname" value="{{ $user->lastName }}">
-                                </div>
-                            </div>
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label for="lastName" class="profileFieldLabel">Lastname</label>
+                            <input class="form-control profileGroup" type="text" name="lastName" placeholder="Lastname" value="{{ $user->lastName }}">
                         </div>
+                    </div>
+                </div>
 
-                        <div class="form-row">
-                            <div class="col">
-                                <div class="form-group" style="color: #011C3A;">
-                                    <label for="email" class="profileFieldLabel">Email</label>
-                                    <input class="form-control" type="email" name="email" placeholder="Email" value="{{ $user->email }}">
-                                </div>
-                            </div>
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label for="email" class="profileFieldLabel">Email</label>
+                            <input class="form-control" type="email" name="email" placeholder="Email" value="{{ $user->email }}">
                         </div>
+                    </div>
+                </div>
 
-                        <div class="form-row">
-                            <div class="col">
-                                <div class="form-group" style="color: #011C3A;">
-                                    <label for="password" class="profileFieldLabel">Password</label>
-                                    <input class="form-control profileGroup" type="password" name="password" placeholder="Password">
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="form-group" style="color: #011C3A;">
-                                    <label for="password_confirmation" class="profileFieldLabel">Confirm Password</label>
-                                    <input class="form-control profileGroup" type="password" name="password_confirmation" placeholder="Confirm password">
-                                </div>
-                            </div>
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label for="password" class="profileFieldLabel">Password</label>
+                            <input class="form-control profileGroup" type="password" name="password" placeholder="Password">
                         </div>
-                        <div class="form-row" style="text-align:right;">
-                            <div class="col offset-xl-0">
-                                <button class="btn btn-primary btnDeclined" type="button" style="margin-right:-6%;">Cancel</button>
-                                <button class="btn btn-primary btnApproved" type="submit" style="margin-right:5%;">Save</button>
-                            </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label for="password_confirmation" class="profileFieldLabel">Confirm Password</label>
+                            <input class="form-control profileGroup" type="password" name="password_confirmation" placeholder="Confirm password">
                         </div>
-            </form>
-        </div>
-    </section>
+                    </div>
+                </div>
+
+                <div class="form-row" style="text-align:right;">
+                    <div class="col offset-xl-0">
+                        <button class="btn btn-primary btnDeclined" type="button" style="margin-right:-6%;">Cancel</button>
+                        <button class="btn btn-primary btnApproved" type="submit" style="margin-right:5%;">Save</button>
+                    </div>
+                </div>
+        </form>
+    </div>
+</section>
 
 
 @isset($user->student)
