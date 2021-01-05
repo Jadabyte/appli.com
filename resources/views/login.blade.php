78b455cc867daf7bbd1df03718a390925d4e94bd
LisaDrse <r0423168@student.thomasmore.be>
2021-01-05 14:05:17 +0100

    Update login.blade.php
    updated css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,25 +8,25 @@
 
     <div class="login-dark">
         <img class="img-fluid logo" src="/applibranding/logoAppli.svg?h=60d8998b2af02b7c83c7ce77b565694b" loading="lazy">
-        <form method="post" action="">
-            <h2 class="text-center headerTwo">Login</h2>
+        <form method="post" action="" class="formRegister" style="margin-top:12.5%">
+            <h1 class="text-center headerTwo">Login</h1>
 
             @csrf
-            @if( $flash = session('message') )
-                <div class="alert alert-success">{{ $flash }}</div>
-            @endif
+                @if( $flash = session('message') )
+                    <div class="alert alert-success">{{ $flash }}</div>
+                @endif
 
-            @if( $flash = session('error') )
-                <div class="alert alert-danger">{{ $flash }}</div>
-            @endif
+                @if( $flash = session('error') )
+                    <div class="alert alert-danger">{{ $flash }}</div>
+                @endif
 
-            @if( $errors->any())
-                <ul class="alert alert-danger">
-                @foreach($errors->all() as $e)
-                    <li>{{ $e }}</li>
-                @endforeach
-                </ul>
-            @endif
+                @if( $errors->any())
+                    <ul class="alert alert-danger">
+                    @foreach($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                    @endforeach
+                    </ul>
+                @endif
 
             <div class="form-group">
                 <input class="form-control" type="email" name="email" placeholder="Email" aria-describedby="emailHelp" value="{{ old('email') }}">
@@ -34,8 +34,13 @@
             <div class="form-group">
                 <input class="form-control" type="password" name="password" placeholder="Password">
             </div>
-            <button class="btn btn-primary btn-block" id="loginbutton" type="submit">Log in</button>
-            <a id="already" class="already" href="register">New here? Go to register page.</a>
+            <div class="form-group" id="form-button-group" style="margin-top:12.5%;">
+                <button class="btn btn-primary btn-block btn" id="registerbutton" type="submit">Log in</button>
+            </div>
+            <div style="margin-top:10%;">
+                <a id="already" class="forgot" href="register">New here? Go to register page.</a>
+            </div>
+
         </form>
     </div>
 @component('components/general/footer')@endcomponent
