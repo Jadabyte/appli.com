40770f01f54106c86dffa9fd9a9a5d5c57e2b087
LisaDrse <r0423168@student.thomasmore.be>
2021-01-05 16:07:14 +0100

    update css

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,8 +5,9 @@
 @endsection
 
 @section('content')
+    <img class="img-fluid logo" src="/applibranding/logoAppli.svg?h=60d8998b2af02b7c83c7ce77b565694b" loading="lazy" id="logoRegister">
+
     <div class="formRegister">
-        <img class="img-fluid logo" src="/applibranding/logoAppli.svg?h=60d8998b2af02b7c83c7ce77b565694b" loading="lazy" id="logoRegister">
         <div class="form-container">
             <form method="post" action="">
                 <h2 class="text-center headerTwo">Create a new account</h2>
