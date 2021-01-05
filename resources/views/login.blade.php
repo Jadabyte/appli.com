2a993ccf894ba190db51e61d5f9ff6f0ac34047c
LisaDrse <r0423168@student.thomasmore.be>
2021-01-05 14:06:31 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,5 +43,17 @@
 
         </form>
     </div>
-@component('components/general/footer')@endcomponent
+<footer id="footerpad" class="footerCreate" style="margin-top:40%;">
+    <div class="container" id="containerFooter">
+        <div class="row">
+            <div class="col-md-6 col-lg-8 mx-auto">
+                <ul class="list-inline text-center">
+                    <li id="social-icons-1" class="list-inline-item"><a href="#"><span class="fa-stack fa-lg" id="facebook"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li id="social-icons-2" class="list-inline-item"><a href="#"><span class="fa-stack fa-lg" id="twitter"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li id="social-icons-3" class="list-inline-item"><a href="#"><span class="fa-stack fa-lg" id="instagram"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-instagram fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li id="social-icons-4" class="list-inline-item"><a href="#"><span class="fa-stack fa-lg" id="pinterest"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-pinterest fa-stack-1x fa-inverse"></i></span></a></li>
+                </ul><p class="copyright text-muted text-center" id="copyright">Copyright &copy; Appli 2020</p></div>
+        </div>
+    </div>
+</footer>
 @endsection
