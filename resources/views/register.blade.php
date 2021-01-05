829a04f273c89fe33a1963f82293c6840b448c1b
LisaDrse <r0423168@student.thomasmore.be>
2021-01-05 14:25:03 +0100

    Update register.blade.php
    update css

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,8 +8,9 @@
 
     <div class="login-dark">
         <img class="img-fluid logo" src="/applibranding/logoAppli.svg?h=60d8998b2af02b7c83c7ce77b565694b" loading="lazy">
-        <form method="post" action="">
-            <h2 class="text-center headerTwo">Register</h2>
+        <div class="form-container">
+            <form class="formRegister" method="post" action="" style="margin-top:20%;">
+                <h2 class="text-center headerTwo">Create a new account</h2>
 
             @csrf
             @if( $flash = session('message') )
@@ -29,27 +30,35 @@
             @endif
 
             <div class="form-group">
-                <input class="form-control" type="text" name="firstName" placeholder="First name" value="{{ old('firstName') }}">
+                <label for="firstName">Firstname</label>
+                <input class="form-control" type="text" name="firstName" placeholder="Firstname" value="{{ old('firstName') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" name="lastName" placeholder="Last name" value="{{ old('lastName') }}">
+                <label for="lastName">Lastname</label>
+                <input class="form-control" type="text" name="lastName" placeholder="Lastname" value="{{ old('lastName') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="email" name="email" placeholder="Email" aria-describedby="emailHelp" value="{{ old('email') }}">
+                <label for="email" aria-describedby="emailHelp">Email</label>
+                <input class="form-control" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
                 <small id="emailHelp" class="form-text text-muted">You can only use an email address from Thomas More.</small>
             </div>
             <div class="form-group">
+                <label for="password">Password</label>
                 <input class="form-control" type="password" name="password" placeholder="Password">
             </div>
-            <div class="form-group form-check">
+            <div class="form-check">
                 <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="isStudent" value="1">
+                    <input class="form-check-input" type="checkbox" name="isStudent" value="1" id="isStudent">
                     <span>I am a student.</span>
                 </label>
             </div>
-            <button class="btn btn-primary btn-block" id="registerbutton" type="submit">Register</button>
+            <div class="form-group">
+                <button class="btn btn-primary btn-block" id="registerbutton" type="submit">Register</button>
+            </div>
+
             <a id="already" class="already" href="login">You already have an account? Login here.</a>
         </form>
     </div>
+</div>
 @component('components/general/footer')@endcomponent
 @endsection
