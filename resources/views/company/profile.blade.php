b62b164e1d69305275a66e94ca84e29e1f8c3ebb
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 21:51:28 +0100

    Update profile.blade.php
    update layout profile company

--- resources/views/company/profile.blade.php
+++ resources/views/company/profile.blade.php
@@ -32,152 +32,175 @@
                         </ul>
                     @endif
 
-                    @isset($user->company->logo)
-                    <div class="profile-header-container">
-                        <div class="profile-header-img">
-                            <img class="rounded mx-auto d-block" style="width:30%" src="{{ asset('storage/companylogos/' . $user->company->logo) }}" />
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group" style="color:#011C3A;">
+                            <label class="profileFieldLabel">Companyname</label>
+                            <input class="form-control profileGroup" type="text" name="companyName" placeholder="Companyname" value="@isset($user->company->name){{ $user->company->name }}@else{{ old('companyName') }}@endisset">
+                            <button class="btn btn-primary searchButton" type="submit" formaction="/company/match">Search</button>
+                            <p class="infoSearch">Please enter the name of your company and we will automically try to determine the rest of your information.</p>
+                            <p class="infoSearch">Please fill in any information that was not automatically found.</p>
+                        </div>
+
+                    <div class="form-group" style="color: #011C3A;">
+                        <label class="profileFieldLabel">Company email</label>
+                        <input class="form-control profileGroup" type="email" name="companyEmail" placeholder="Company email" value="@isset($user->company->mail){{ $user->company->mail }}@else{{ old('companyEmail') }}@endisset">
+                    </div>
+
+                    <div class="form-group" style="color: #011C3A;">
+                        <label class="profileFieldLabel">Company phone</label>
+                        <input class="form-control profileGroup" type="tel" name="phone" placeholder="Company telephone" value="@isset($user->company->telephone){{ $user->company->telephone }}@else{{ old('phone') }}@endisset">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="avatar">
+                        <div class="avatar-bg center" style="margin-left:35%;">
+                            @if(isset($user->company->logo))
+                            <div class="profile-header-container">
+                                <div class="profile-header-img">
+                                    <img class="rounded mx-auto d-block" style="width:30%" src="{{ asset('storage/companylogos/' . $user->company->logo) }}" />
+                                </div>
+                            </div>
+                            @else
+                            <div class="profile-header-container">
+                                <div class="profile-header-img">
+                                    <img class="rounded mx-auto d-block" style="width:80%" src="../public/images/students/profile.png" />
+                                </div>
+                            </div>
+                            @endif
+                            <div style="margin-top:1%; color: #011C3A;">
+                                <label for="picture" class="profileFieldLabel">Company logo</label>
+                                <input type="file" class="form-control" name="picture" style="color: #011C3A;" aria-describedby="fileHelp">
+                                <p class="infoSearch" id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</p>
+                            </div>
                         </div>
                     </div>
-                    @endisset
+                </div>
+            </div>
 
-                    <div class="form-row" style="margin-top:-5%">
-                        <div class="col">
-                            <div class="avatar">
-                                <div class="avatar-bg center" style="margin-left:35%;"></div>
-                                    <div style="margin-top:1%; color: #011C3A;">
-                                        <label class="profileFieldLabel">Company Logo</label>
-                                        <p class="form-text text-muted infoSearch" id="fileHelp">Please upload a valid image file. Size of image should not be more than 2MB.</p>
-                                    </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="form-group" style="color:#011C3A;">
-                                <label class="profileFieldLabel">Companyname</label>
-                                <input class="form-control profileGroup" type="text" name="companyName" placeholder="Companyname" value="@isset($user->company->name){{ $user->company->name }}@else{{ old('companyName') }}@endisset">
-                                <button class="btn btn-primary searchButton" type="submit" formaction="/company/match">Search</button>
-                                <p class="infoSearch">Please enter the name of your company and we will automically try to determine the rest of your information.</p>
-                                <p class="infoSearch">Please fill in any information that was not automatically found.</p>
-                            </div>
-
+                <div class="form-row">
+                    <div class="col">
                         <div class="form-group" style="color: #011C3A;">
-                            <label class="profileFieldLabel">Company email</label>
-                            <input class="form-control profileGroup" type="email" name="companyEmail" placeholder="Company email" value="@isset($user->company->mail){{ $user->company->mail }}@else{{ old('companyEmail') }}@endisset">
-                        </div>
+                            <label class="profileFieldLabel">Description of company</label>
+                        <textarea class="form-control profileGroup" name="description" placeholder="Description">@isset($user->company->description){{ $user->company->description }}@else{{ old('description') }}@endisset</textarea>
+                    </div>
+                </div>
 
+                <div class="form-row">
+                    <div class="col">
                         <div class="form-group" style="color: #011C3A;">
-                            <label class="profileFieldLabel">Company phone</label>
-                            <input class="form-control profileGroup" type="tel" name="phone" placeholder="Company telephone" value="@isset($user->company->telephone){{ $user->company->telephone }}@else{{ old('phone') }}@endisset">
-                        </div>
+                            <label class="profileFieldLabel">Company website</label>
+                            <input class="form-control profileGroup" type="url" name="website" placeholder="Website" value="@isset($user->company->website){{ $user->company->website }}@else{{ old('website') }}@endisset">
+                    </div>
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label class="profileFieldLabel">Company LinkedIn</label>
+                            <input class="form-control profileGroup" type="url" name="linkedin" placeholder="LinkedIn" value="@isset($user->company->LinkedIn){{ $user->company->LinkedIn }}@else{{ old('linkedin') }}@endisset">
+                    </div>
+                </div>
 
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label for="category" class="profileFieldLabel">Category</label>
+                            <select class="form-control" name="category">
+                                @foreach ($categories as $category)
+                                        <option @if(!empty($user->company->category_id) && $category->id === $user->company->category_id){{ 'selected' }}@endif value="{{ $category->id }}">{{ $category->title }}</option>
+                                @endforeach
+                            </select>
                         </div>
                     </div>
+                </div>
 
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">Description of company</label>
-                            <textarea class="form-control profileGroup" name="description" placeholder="Description">@isset($user->company->description){{ $user->company->description }}@else{{ old('description') }}@endisset</textarea>
-                        </div>
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label class="profileFieldLabel">Street</label>
+                        <input class="form-control profileGroup" type="text" name="street" placeholder="Street" value="@isset($user->company->street){{ $user->company->street }}@else{{ old('street') }}@endisset">
                     </div>
 
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">Company website</label>
-                                <input class="form-control profileGroup" type="url" name="website" placeholder="Website" value="@isset($user->company->website){{ $user->company->website }}@else{{ old('website') }}@endisset">
-                        </div>
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">Company LinkedIn</label>
-                                <input class="form-control profileGroup" type="url" name="linkedin" placeholder="LinkedIn" value="@isset($user->company->LinkedIn){{ $user->company->LinkedIn }}@else{{ old('linkedin') }}@endisset">
-                        </div>
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label class="profileFieldLabel">House number</label>
+                        <input class="form-control" type="number" name="houseNumber" placeholder="House number" value="@isset($user->company->houseNumber){{ $user->company->houseNumber }}@else{{ old('houseNumber') }}@endisset">
                     </div>
-
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label for="category" class="profileFieldLabel">Category</label>
-                                <select class="form-control" name="category">
-                                    @foreach ($categories as $category)
-                                            <option @if(!empty($user->company->category_id) && $category->id === $user->company->category_id){{ 'selected' }}@endif value="{{ $category->id }}">{{ $category->title }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label class="profileFieldLabel">P.O. Box</label>
+                        <input class="form-control" type="number" name="pobox" placeholder="P.O. box" value="@isset($user->company->pobox){{ $user->company->pobox }}@else{{ old('pobox') }}@endisset">
                     </div>
+                </div>
 
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">Street</label>
-                            <input class="form-control profileGroup" type="text" name="street" placeholder="Street" value="@isset($user->company->street){{ $user->company->street }}@else{{ old('street') }}@endisset">
-                        </div>
-
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">House number</label>
-                            <input class="form-control" type="number" name="houseNumber" placeholder="House number" value="@isset($user->company->houseNumber){{ $user->company->houseNumber }}@else{{ old('houseNumber') }}@endisset">
-                        </div>
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">P.O. Box</label>
-                            <input class="form-control" type="number" name="pobox" placeholder="P.O. box" value="@isset($user->company->pobox){{ $user->company->pobox }}@else{{ old('pobox') }}@endisset">
-                        </div>
+                <div class="form-row">
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label class="profileFieldLabel">Postal Code</label>
+                        <input class="form-control" type="number" name="postalCode" placeholder="Postal code" value="@isset($user->company->postalCode){{ $user->company->postalCode }}@else{{ old('postalCode') }}@endisset">
                     </div>
-
-                    <div class="form-row">
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">Postal Code</label>
-                            <input class="form-control" type="number" name="postalCode" placeholder="Postal code" value="@isset($user->company->postalCode){{ $user->company->postalCode }}@else{{ old('postalCode') }}@endisset">
-                        </div>
-                        <div class="col">
-                            <div class="form-group" style="color: #011C3A;">
-                                <label class="profileFieldLabel">City</label>
-                            <input class="form-control" type="text" name="city" placeholder="City" value="@isset($user->company->city){{ $user->company->city }}@else{{ old('city') }}@endisset">
-                        </div>
+                    <div class="col">
+                        <div class="form-group" style="color: #011C3A;">
+                            <label class="profileFieldLabel">City</label>
+                        <input class="form-control" type="text" name="city" placeholder="City" value="@isset($user->company->city){{ $user->company->city }}@else{{ old('city') }}@endisset">
                     </div>
+                </div>
+                <div class="form-row" style="text-align:right;">
+                    <div class="col offset-xl-0">
                     <button class="btn btn-primary btnApproved" type="submit">Save</button>
+                    </div>
                 </form>
             </div>
         </section>
 
 <section style="margin-top:85%;">
-    <h3 class="headerThree">Accountdetails</h3>
-        <div class="profileForm" style="margin-top:17.5%">
-        <form method="post" action="">
+    <h3 class="headerThree" style="margin-left:-16%;">Accountdetails</h3>
+        <div class="profileForm" id="accountForm" style="margin-top:17.5%">
+        <form class="profileContainer" method="post" action="" style="margin-top:8%;">
             @csrf
-
-            <div class="form-row">
+            <div class="form-row" style="margin-top:-10%">
                 <div class="col">
-                    <label for="firstName">First name</label>
-                    <input class="form-control" type="text" name="firstName" placeholder="First name" value="{{ $user->firstName }}">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="firstName" class="profileFieldLabel">Firstname</label>
+                        <input class="form-control profileGroup" type="text" name="firstName" placeholder="Firstname" value="{{ $user->firstName }}">
+                    </div>
                 </div>
                 <div class="col">
-                    <label for="lastName">Last name</label>
-                    <input class="form-control" type="text" name="lastName" placeholder="Last name" value="{{ $user->lastName }}">
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="col">
-                    <label for="email">Email</label>
-                    <input class="form-control" type="email" name="email" placeholder="Email" value="{{ $user->email }}">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="lastName" class="profileFieldLabel">Lastname</label>
+                        <input class="form-control profileGroup" type="text" name="lastName" placeholder="Lastname" value="{{ $user->lastName }}">
+                    </div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
-                    <label for="password">Password</label>
-                    <input class="form-control" type="password" name="password" placeholder="Password">
-                </div>
-                <div class="col">
-                    <label for="password_confirmation">Confirm password</label>
-                    <input class="form-control" type="password" name="password_confirmation" placeholder="Confirm password">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="email" class="profileFieldLabel">Email</label>
+                        <input class="form-control profileGroup" type="email" name="email" placeholder="Email" value="{{ $user->email }}">
+                    </div>
                 </div>
             </div>
-            <button class="btn btn-primary" type="submit">Save</button>
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="password" class="profileFieldLabel">Password</label>
+                        <input class="form-control profileGroup" type="password" name="password" placeholder="Password">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group" style="color: #011C3A;">
+                        <label for="password_confirmation" class="profileFieldLabel">Confirm password</label>
+                        <input class="form-control profileGroup" type="password" name="password_confirmation" placeholder="Confirm password">
+                    </div>
+                </div>
+            </div>
+            <div class="form-row" style="text-align:right;">
+                <div class="col offset-xl-0">
+                    <button class="btn btn-primary btnApproved" type="submit">Save</button>
+                </div>
+            </div>
         </form>
     </div>
 </div>
+@endisset
     @component('components/general/footer')@endcomponent
     @endsection
