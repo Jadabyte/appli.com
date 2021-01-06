cb51cc48879b5528d6a32e0f592ab91faed9eca8
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 14:05:42 +0100

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -14,7 +14,7 @@
                     <div class="border rounded shadow" style="background-color: #f97242;"><img class="img-fluid" src="../assets/img/html.svg"></div>
                     <div class="card-body text-center">
                         <h3 class="card-title">{{$i->title}}</h3>
-                        <h4 class="card-title headerThree">company id:{{$i->company_name}}</h4>
+                        {{-- <h4 class="card-title headerThree">company id:{{$i->company_title}}</h4> --}}
                         <p class="explanationParagraph">{{$i->description}}</p>
                         <button class="btn btn-light active btn-block moreButton" type="button"><a href="/internship/{{$i->id}}">More</a></button>
                     </div>
