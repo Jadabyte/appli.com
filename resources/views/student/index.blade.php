6616a4d42ff4021292cf4b4f3e056464e990dca2
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 14:09:38 +0100

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -11,10 +11,10 @@
         <div class="row d-flex d-sm-flex justify-content-center justify-content-sm-center">
             <div class="col-md-3">
                 <div class="card border rounded shadow">
-                    <div class="border rounded shadow" style="background-color: #f97242;"><img class="img-fluid" src="../assets/img/html.svg"></div>
+                    <div class="border rounded shadow" style="background-color: #f97242;">{{-- <img class="img-fluid" src="../assets/img/html.svg"> --}}</div>
                     <div class="card-body text-center">
                         <h3 class="card-title">{{$i->title}}</h3>
-                        {{-- <h4 class="card-title headerThree">company id:{{$i->company_title}}</h4> --}}
+                        <h4 class="card-title headerThree">company id:{{$i->company_title}}</h4>
                         <p class="explanationParagraph">{{$i->description}}</p>
                         <button class="btn btn-light active btn-block moreButton" type="button"><a href="/internship/{{$i->id}}">More</a></button>
                     </div>
