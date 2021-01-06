1310f062bc059b89430a93c3eaf52cd5ebb68e5c
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 14:03:25 +0100

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -6,6 +6,21 @@
 
 @section('content')
     @component('components/student/filters')@endcomponent
-    @component('components/internship/showInternships')@endcomponent
+
+    @foreach ($internships as $i)
+        <div class="row d-flex d-sm-flex justify-content-center justify-content-sm-center">
+            <div class="col-md-3">
+                <div class="card border rounded shadow">
+                    <div class="border rounded shadow" style="background-color: #f97242;"><img class="img-fluid" src="../assets/img/html.svg"></div>
+                    <div class="card-body text-center">
+                        <h3 class="card-title">{{$i->title}}</h3>
+                        <h4 class="card-title headerThree">company id:{{$i->company_name}}</h4>
+                        <p class="explanationParagraph">{{$i->description}}</p>
+                        <button class="btn btn-light active btn-block moreButton" type="button"><a href="/internship/{{$i->id}}">More</a></button>
+                    </div>
+                </div>
+            </div>
+    @endforeach
+
     @component('components/general/footer')@endcomponent
 @endsection
