a065ef8d304ce639a2e9147b6f67db3d4321bdea
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 21:58:16 +0100

    Update profile.blade.php

--- resources/views/company/profile.blade.php
+++ resources/views/company/profile.blade.php
@@ -74,6 +74,7 @@
                                 <input type="file" class="form-control" name="picture" style="color: #011C3A;" aria-describedby="fileHelp">
                                 <p class="infoSearch" id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</p>
                             </div>
+                            @endisset
                         </div>
                     </div>
                 </div>
@@ -150,7 +151,6 @@
                     </div>
                 </form>
             </div>
-            @endisset
         </section>
 
 <section style="margin-top:85%;">
