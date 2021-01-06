acf988b8c8fa40bd95e3d973bed1fc37ddd1e93b
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 20:03:06 +0100

    update layout and pages

--- resources/views/components/student/filters.blade.php
+++ resources/views/components/student/filters.blade.php
@@ -8,65 +8,65 @@
             </div>
     <div class="filter">
         <form>
-            <select>
-                <option value="">Category</option>
-                <option value="">Hybrid</option>
-                <option value="">Designer</option>
-                <option value="">Devigner</option>
+            <select class="filterDrop">
+                <option class="filterTags" value="">Category</option>
+                <option class="filterTags" value="">Hybrid</option>
+                <option class="filterTags" value="">Designer</option>
+                <option class="filterTags" value="">Devigner</option>
             </select>
-            <select>
-                <option value="">Location</option>
-                <option value="">&lt; 5km</option>
-                <option value="">&gt; 5km / &lt; 10km</option>
-                <option value="">&gt; 10km</option>
+            <select class="filterDrop">
+                <option class="filterTags" value="">Location</option>
+                <option class="filterTags" value="">&lt; 5km</option>
+                <option class="filterTags" value="">&gt; 5km / &lt; 10km</option>
+                <option class="filterTags" value="">&gt; 10km</option>
             </select>
-            <select>
-                <option value="">Skills</option>
-                    <option value="">Laravel</option>
-                    <option value="">Linux</option>
-                    <option value="">PHP</option>
-                    <option value="">CSS</option>
-                    <option value="">HTML</option>
-                    <option value="">Javascript</option>
-                    <option value="">NodeJS</option>
-                    <option value="">Sass</option>
-                    <option value="">ASP.NET Core</option>
-                    <option value="">React</option>
-                    <option value="">JSON</option>
-                    <option value="">API's</option>
-                    <option value="">Git</option>
-                    <option value="">GitHub</option>
-                    <option value="">VueJS</option>
-                    <option value="">Websockets</option>
-                    <option value="">Gulp</option>
-                    <option value="">Parcel</option>
-                    <option value="">Babel</option>
-                    <option value="">Java</option>
-                    <option value="">MongoDB</option>
-                    <option value="">CMS</option>
-                    <option value="">Drupal</option>
-                    <option value="">Arduino</option>
-                    <option value="">Information Architecture</option>
-                    <option value="">User Interface Design</option>
-                    <option value="">User Experience</option>
-                    <option value="">Sketch</option>
-                    <option value="">Procreate</option>
-                    <option value="">Adobe Illustrator</option>
-                    <option value="">Adobe Photoshop</option>
-                    <option value="">Adobe Indesign</option>
-                    <option value="">Adobe Premiere Pro</option>
-                    <option value="">Adobe XD</option>
-                    <option value="">Wonda</option>
-                    <option value="">Kanban</option>
-                    <option value="">SCRUM</option>
-                    <option value="">Agile</option>
-                    <option value="">Trello</option>
+            <select class="filterDrop">
+                <option class="filterTags" value="">Skills</option>
+                    <option class="filterTags" value="">Laravel</option>
+                    <option class="filterTags" value="">Linux</option>
+                    <option class="filterTags" value="">PHP</option>
+                    <option class="filterTags" value="">CSS</option>
+                    <option class="filterTags" value="">HTML</option>
+                    <option class="filterTags" value="">Javascript</option>
+                    <option class="filterTags" value="">NodeJS</option>
+                    <option class="filterTags" value="">Sass</option>
+                    <option class="filterTags" value="">ASP.NET Core</option>
+                    <option class="filterTags" value="">React</option>
+                    <option class="filterTags" value="">JSON</option>
+                    <option class="filterTags" value="">API's</option>
+                    <option class="filterTags" value="">Git</option>
+                    <option class="filterTags" value="">GitHub</option>
+                    <option class="filterTags" value="">VueJS</option>
+                    <option class="filterTags" value="">Websockets</option>
+                    <option class="filterTags" value="">Gulp</option>
+                    <option class="filterTags" value="">Parcel</option>
+                    <option class="filterTags" value="">Babel</option>
+                    <option class="filterTags" value="">Java</option>
+                    <option class="filterTags" value="">MongoDB</option>
+                    <option class="filterTags" value="">CMS</option>
+                    <option class="filterTags" value="">Drupal</option>
+                    <option class="filterTags" value="">Arduino</option>
+                    <option class="filterTags" value="">Information Architecture</option>
+                    <option class="filterTags" value="">User Interface Design</option>
+                    <option class="filterTags" value="">User Experience</option>
+                    <option class="filterTags" value="">Sketch</option>
+                    <option class="filterTags" value="">Procreate</option>
+                    <option class="filterTags" value="">Adobe Illustrator</option>
+                    <option class="filterTags" value="">Adobe Photoshop</option>
+                    <option class="filterTags" value="">Adobe Indesign</option>
+                    <option class="filterTags" value="">Adobe Premiere Pro</option>
+                    <option class="filterTags" value="">Adobe XD</option>
+                    <option class="filterTags" value="">Wonda</option>
+                    <option class="filterTags" value="">Kanban</option>
+                    <option class="filterTags" value="">SCRUM</option>
+                    <option class="filterTags" value="">Agile</option>
+                    <option class="filterTags" value="">Trello</option>
             </select>
-            <select>
-                <option value="">Company Size</option>
-                <option value="">&lt; 10 employees</option>
-                <option value="">&gt; 10 employees / &lt; 50 employees</option>
-                <option value="">&gt; 50 employees</option>
+            <select class="filterDrop">
+                <option class="filterTags" value="">Company Size</option>
+                <option class="filterTags" value="">&lt; 10 employees</option>
+                <option class="filterTags" value="">&gt; 10 employees / &lt; 50 employees</option>
+                <option class="filterTags" value="">&gt; 50 employees</option>
             </select>
             <a class="resetFilter" href="#">Reset filters</a>
         </form>
