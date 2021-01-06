0bc8dba1de744d2ae3181f890cf92b3632479532
LisaDrse <r0423168@student.thomasmore.be>
2021-01-06 20:36:38 +0100

    update catalogue internship

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -21,6 +21,31 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-3">
+                <div class="card border rounded shadow">
+                    <div class="card-body text-center">
+                        <h3 class="card-title">{{$i->title}}</h3>
+                        <h4 class="card-title headerThree">{{$i->company->name}}</h4>
+                        <p class="explanationParagraph">{{$i->description}}</p>
+                        <button class="btn btn-light active btn-block moreButton" type="button">
+                            <a href="/internship/{{$i->id}}">More</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border rounded shadow">
+                    <div class="card-body text-center">
+                        <h3 class="card-title">{{$i->title}}</h3>
+                        <h4 class="card-title headerThree">{{$i->company->name}}</h4>
+                        <p class="explanationParagraph">{{$i->description}}</p>
+                        <button class="btn btn-light active btn-block moreButton" type="button">
+                            <a href="/internship/{{$i->id}}">More</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endforeach
 
     @component('components/general/footer')@endcomponent
