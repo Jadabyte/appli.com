d4d2229924d3eb7628380f39f2f74a58be9659fc
LisaDrse <r0423168@student.thomasmore.be>
2021-01-07 17:10:39 +0100

    Update index.blade.php

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="card border rounded shadow">
                     <div class="card-body text-center">
                         <h3 class="card-title">{{$i->title}}</h3>
-                        <h4 class="card-title headerThree">{{$i->company->name}}</h4>
+                        <h4 class="card-title headerThree">{{$i->company_id->name}}</h4>
                         <p class="explanationParagraph">{{$i->description}}</p>
                         <button class="btn btn-light btn-block moreButton" style="text-transform:capitalize" type="button">
                             <a href="/internship/{{$i->id}}">More</a>
