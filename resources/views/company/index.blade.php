67c84017b215e2620c4563024e5b3a1c6294408d
LisaDrse <r0423168@student.thomasmore.be>
2021-01-07 17:21:12 +0100

    Update index.blade.php

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -16,7 +16,7 @@
                 <div class="card-body text-center">
                     <h3 class="card-title">{{$u->firstName}}{{$u->lastName}}</h3>
                     <button class="btn btn-light btn-block moreButton" style="text-transform:capitalize" type="button">
-                        <a href="/student/{{$u->id}}">More</a></button>
+                        <a href="/student/{{$u->id}}" style="color:#EAEAEA;">More</a></button>
                 </div>
             </div>
         </div>
