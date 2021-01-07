1a719bfd5f0fa718321b06962ab1ce9a8676597a
LisaDrse <r0423168@student.thomasmore.be>
2021-01-07 14:53:40 +0100

    Update profile.blade.php

--- resources/views/company/profile.blade.php
+++ resources/views/company/profile.blade.php
@@ -150,7 +150,7 @@
 
 <section style="margin-top:95%;">
     <h3 class="headerThree">Accountdetails</h3>
-        <div class="profileForm" id="accountForm" style="margin-top:14.5%; margin-bottom:15%;">
+        <div class="profileForm" id="accountForm" style="margin-top:12.5%; margin-bottom:15%;">
         <form class="profileContainer" method="post" action="" style="margin-top:8%;">
             @csrf
             <div class="form-row" style="margin-top:-10%">
