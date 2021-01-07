4feb3dba983fc809191087bebf421c556d2d4b56
LisaDrse <r0423168@student.thomasmore.be>
2021-01-07 15:03:33 +0100

    Update index.blade.php

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -17,10 +17,10 @@
         <div class="col-md-3">
             <div class="card border rounded shadow">
                 <div class="card-body text-center">
-                    <h3 class="card-title">{{$u->firstname}}{{$u->lastname}}</h3>
-                    <p class="explanationParagraph">{{$u->biography}}</p>
+                    <h3 class="card-title">{{$u->student->firstname}}{{$u->student->lastname}}</h3>
+                    <p class="explanationParagraph">{{$u->student->biography}}</p>
                     <button class="btn btn-light btn-block moreButton" type="button">
-                        <a href="/student/{{$u->id}}">More</a></button>
+                        <a href="/student/{{$u->student->id}}">More</a></button>
                 </div>
             </div>
         </div>
